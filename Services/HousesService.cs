ffeb2d51bd9c68a995089144a668634671313666
trevor-r-allen <tallen2699@gmail.com>
2021-02-17 16:03:31 -0700

    services updated

--- Services/HousesService.cs
+++ Services/HousesService.cs
@@ -1,7 +1,56 @@
+using System;
+using System.Collections.Generic;
+using csharp_gregslist.DB;
+using csharp_gregslist.Models;
+
 namespace csharp_gregslist.Services
 {
-    public class HousesService
+  public class HousesService
+  {
+    public IEnumerable<House> Get()
     {
-        
+      return FAKEDB.Houses;
     }
+
+    public House Get(string houseId)
+    {
+      House house = FAKEDB.Houses.Find(c => c.Id == houseId);
+      if (house == null)
+      {
+        throw new Exception("Invalid Id");
+      }
+      return house;
+    }
+
+    public House Create(House newhouse)
+    {
+      FAKEDB.Houses.Add(newhouse);
+      return newhouse;
+    }
+
+    public House Edit(House editedhouse)
+    {
+      House house = FAKEDB.Houses.Find(c => c.Id == editedhouse.Id);
+      if (house == null)
+      {
+        throw new Exception("Invalid Id");
+      }
+      FAKEDB.Houses.Remove(house);
+      FAKEDB.Houses.Add(editedhouse);
+      return editedhouse;
+    }
+
+    public string Delete(string houseId)
+    {
+      House house = FAKEDB.Houses.Find(c => c.Id == houseId);
+      if (house == null)
+      {
+        throw new Exception("Invalid Id");
+      }
+      FAKEDB.Houses.Remove(house);
+      return "Successfully Deleted";
+    }
+
+
+  }
 }
--- Services/JobsService.cs
+++ Services/JobsService.cs
@@ -1,7 +1,56 @@
+using System;
+using System.Collections.Generic;
+using csharp_gregslist.DB;
+using csharp_gregslist.Models;
+
 namespace csharp_gregslist.Services
 {
-    public class JobsService
+  public class JobsService
+  {
+    public IEnumerable<Job> Get()
     {
-        
+      return FAKEDB.Jobs;
     }
+
+    public Job Get(string jobId)
+    {
+      Job job = FAKEDB.Jobs.Find(c => c.Id == jobId);
+      if (job == null)
+      {
+        throw new Exception("Invalid Id");
+      }
+      return job;
+    }
+
+    public Job Create(Job newjob)
+    {
+      FAKEDB.Jobs.Add(newjob);
+      return newjob;
+    }
+
+    public Job Edit(Job editedjob)
+    {
+      Job job = FAKEDB.Jobs.Find(c => c.Id == editedjob.Id);
+      if (job == null)
+      {
+        throw new Exception("Invalid Id");
+      }
+      FAKEDB.Jobs.Remove(job);
+      FAKEDB.Jobs.Add(editedjob);
+      return editedjob;
+    }
+
+    public string Delete(string jobId)
+    {
+      Job job = FAKEDB.Jobs.Find(c => c.Id == jobId);
+      if (job == null)
+      {
+        throw new Exception("Invalid Id");
+      }
+      FAKEDB.Jobs.Remove(job);
+      return "Successfully Deleted";
+    }
+
+
+  }
 }
